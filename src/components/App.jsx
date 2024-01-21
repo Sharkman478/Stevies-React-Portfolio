b07069ba1efd6f0ff51724d384a5f769ea3946b5
Sharkman478 <steviehelber@gmail.com>
2024-01-21 17:46:36 -0500

    getting set up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,21 @@
-import './App.css';
-import BucketList from './components/BucketList';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/UI/Header/Header.jsx";
+import Nav from "./components/UI/Header/Navigation/Navigation.jsx";
+import Footer from "./components/UI/Footer/Footer.jsx";
+
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
+    <>
+      <Header>
+        <Nav />
+      </Header>
+      <Footer />
+      <useState />
+    </>
   );
 }
 
