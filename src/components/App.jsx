2fb83ecf8b5c38791f31644265f9495cdbf7aec7
Sharkman478 <steviehelber@gmail.com>
2024-01-22 18:22:17 -0500

    changes to routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import Header from "./components/UI/Header/Header.jsx";
 import Nav from "./components/UI/Header/Navigation/Navigation.jsx";
 import Footer from "./components/UI/Footer/Footer.jsx";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +13,6 @@ function App() {
         <Nav />
       </Header>
       <Footer />
-      <useState />
     </>
   );
 }
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Pages from "../components/pages/index.jsx";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import AboutMe from "./pages/AboutMe.jsx";
+import Contact from "./pages/Contact.jsx";
+import Resume from "./pages/Resume.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
-const { AboutMe, Contact, Portfolio, Resume} = Pages;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "AboutMe",
+        element: <AboutMe />,
+      },
+      {
+        path: "Contact",
+        element: <Contact />,
+      },
+      {
+        path: "Resume",
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
